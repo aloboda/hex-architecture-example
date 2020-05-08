effd1a180e2acb8a9fd458c20c66d3a004efb861
Andrii Loboda <andrii.loboda@kooltra.com>
2020-05-07 22:52:13 -0400

    Add Spring to run controller.

--- src/main/java/buckpal/Application.java
+++ src/main/java/buckpal/Application.java
@@ -1,7 +1,14 @@
 package buckpal;
 
-public class Application {
-	public static void main(String[] args) {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-	}
+/**
+ * Entry point of application
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class);
+    }
 }
--- src/main/java/buckpal/account/adapter/in/web/AccountController.java
+++ src/main/java/buckpal/account/adapter/in/web/AccountController.java
@@ -1,4 +1,21 @@
 package buckpal.account.adapter.in.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class AccountController {
+
+    @RequestMapping(path = "/send-money", method = RequestMethod.PUT)
+    public void sendMoney() {
+
+    }
+
+    @GetMapping
+    public String helloGradle() {
+        return "Hello Gradle!";
+    }
 }
